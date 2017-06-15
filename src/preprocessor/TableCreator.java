cb93b4bcdef768d7c3a5f2fca7796fdf4a1c845f
Sina <sina.hajimiri@gmail.com>
2017-06-15 23:47:27 +0430

    create parse table

--- src/preprocessor/TableCreator.java
+++ src/preprocessor/TableCreator.java
@@ -4,10 +4,7 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class TableCreator {
     private ArrayList<String> rules = new ArrayList<>();
@@ -145,16 +142,200 @@ public class TableCreator {
         }
     }
 
+    private Table createTable() {
+        Table table = new Table(terminals, nonTerminals);
+        ArrayList<State> states = new ArrayList<>();
+        State s0 = new State(new Item("$S → $Program", 0), rules);
+        states.add(s0);
+
+        for (int i = 0; i < states.size(); i++) {
+            State state = states.get(i);
+            for (String terminal : terminals) {  // Shift
+                Item[] kernel = state.cloneItemsWithDotBeforeTNTAndMoveDot(terminal);
+                if (kernel.length == 0)
+                    continue;
+                State newState = new State(kernel, rules);
+                int newStateNum;
+                if (!states.contains(newState)) {
+                    newStateNum = states.size();
+                    states.add(newState);
+                } else
+                    newStateNum = states.indexOf(newState);
+                table.put("S" + newStateNum, i, terminal);
+            }
+            for (String nonTerminal : nonTerminals) {  // Goto
+                Item[] kernel = state.cloneItemsWithDotBeforeTNTAndMoveDot(nonTerminal);
+                if (kernel.length == 0)
+                    continue;
+                State newState = new State(kernel, rules);
+                int newStateNum;
+                if (!states.contains(newState)) {
+                    newStateNum = states.size();
+                    states.add(newState);
+                } else
+                    newStateNum = states.indexOf(newState);
+                table.put("" + newStateNum, i, nonTerminal);
+            }
+
+           for (Item item : state.items) {  // Reduce
+                if (item.lhs.equals("$S") && item.dotBefore == 1) {
+                    table.put("Acc", i, "EOF");
+                    continue;
+                }
+                if (item.rhs.length == item.dotBefore)
+                    for (String follow : follows.get(item.lhs))
+                        table.put("R" + item.ruleNumber, i, follow);
+            }
+        }
+        return table;
+    }
+
     private void run() {
         readGrammarFile();
         calculateFirsts();
         calculateFollows();
 
-        System.out.println("Firsts:");
-        printFirstsOrFollows(firsts);
-        System.out.println("\n---\n");
-        System.out.println("Follows:");
-        printFirstsOrFollows(follows);
+        Table table = createTable();
+        System.out.println();
+
+//        System.out.println("Follows:");
+//        printFirstsOrFollows(follows);
 
     }
+}
+
+class Item {
+    String lhs;
+    String[] rhs;
+    int dotBefore;
+    int ruleNumber;
+
+    public Item(String lhs, String[] rhs, int dotBefore, int ruleNumber) {
+        this.lhs = lhs;
+        this.rhs = rhs;
+        this.dotBefore = dotBefore;
+        this.ruleNumber = ruleNumber;
+    }
+
+    public Item(String rule, int ruleNumber) {
+        this.lhs = rule.split("→")[0].trim();
+        this.rhs = rule.split("→")[1].trim().split(" ");
+        if (this.rhs[0].equals("ε"))
+            this.rhs = new String[0];
+        this.dotBefore = 0;
+        this.ruleNumber = ruleNumber;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof Item))
+            return false;
+        Item i = (Item) obj;
+        if (!i.lhs.equals(this.lhs) || i.dotBefore != this.dotBefore || i.rhs.length != this.rhs.length)
+            return false;
+        for (int k = 0; k < i.rhs.length; k++)
+            if (!i.rhs[k].equals(this.rhs[k]))
+                return false;
+        return true;
+    }
+
+    public Item clone() {
+        return new Item(lhs, rhs, dotBefore, ruleNumber);
+    }
+
+}
+
+class State {
+    ArrayList<String> grammarRules;
+    ArrayList<Item> items = new ArrayList<>();
+
+    public State(Item[] kernel, ArrayList<String> grammarRules) {
+        this.grammarRules = grammarRules;
+        items.addAll(Arrays.asList(kernel));
+        closure();
+    }
+
+    public State(Item kernel, ArrayList<String> grammarRules) {
+        this.grammarRules = grammarRules;
+        items.add(kernel);
+        closure();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null || !(obj instanceof State))
+            return false;
+        State s = (State) obj;
+        for (Item item : s.items)
+            if (!this.items.contains(item))
+                return false;
+        return true;
+    }
+
+    private void closure() {
+        int i = 0;
+        while (i < items.size()) {
+            HashSet<String> shouldAddRulesOf = new HashSet<>();
+            for (; i < items.size(); i++) {
+                Item current = items.get(i);
+                if (current.rhs.length == current.dotBefore)
+                    continue;
+                if (current.rhs[current.dotBefore].startsWith("$"))
+                    shouldAddRulesOf.add(current.rhs[current.dotBefore]);
+            }
+            for (int j = 0; j < grammarRules.size(); j++) {
+                String rule = grammarRules.get(j);
+                String lhs = rule.split("→")[0].trim();
+                if (shouldAddRulesOf.contains(lhs)) {
+                    Item newItem = new Item(rule, j + 1);  // Number 0 is S → Program
+                    if (!items.contains(newItem))
+                        items.add(newItem);
+                }
+            }
+        }
+    }
+
+    // TODO ghable hameye item.rhs[item.dotBefore] ha bayad check kard noghte akhar nabashe
+    public Item[] cloneItemsWithDotBeforeTNTAndMoveDot(String tnt) {
+        ArrayList<Item> toReturn = new ArrayList<>();
+        for (Item item : items) {
+            if (item.rhs.length == item.dotBefore)
+                continue;
+            if (item.rhs[item.dotBefore].equals(tnt)) {
+                Item newItem = item.clone();
+                newItem.dotBefore++;
+                toReturn.add(newItem);
+            }
+        }
+        return toReturn.toArray(new Item[toReturn.size()]);
+    }
+}
+
+class Table {
+    final int MAX_STATES = 200;
+    HashSet<String> terminals;
+    HashSet<String> nonTerminals;
+    HashMap<String, Integer> tntToColumnNumber = new HashMap<>();
+    String[][] table;
+
+    public Table(HashSet<String> terminals, HashSet<String> nonTerminals) {
+        this.terminals = terminals;
+        this.nonTerminals = nonTerminals;
+        table = new String[MAX_STATES][terminals.size() + nonTerminals.size()];
+        int i = 0;
+        for (String terminal : terminals)
+            tntToColumnNumber.put(terminal, i++);
+        for (String nonTerminal : nonTerminals)
+            tntToColumnNumber.put(nonTerminal, i++);
+    }
+
+    public void put(String text, int state, String tnt) {
+        int column = tntToColumnNumber.get(tnt);
+        if (table[state][column] != null)
+            table[state][column] += text;
+        else
+            table[state][column] = text;
+    }
+
+
 }
