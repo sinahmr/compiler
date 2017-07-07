19ed8c07b787b8e3117034c2339dc5c672e10fb7
Sina <sina.hajimiri@gmail.com>
2017-07-08 01:06:20 +0430

    refactor

--- src/Compiler.java
+++ src/Compiler.java
@@ -18,16 +18,5 @@ public class Compiler {
         }
         CodeGenerator codeGenerator = new CodeGenerator(scanner.getSymbolTable());
         new Parser(scanner, codeGenerator, errorHandler).parse();
-
-        //codeGenerator.run();
-
-
-        /*while(true)
-        {
-            Token next = scanner.getNextToken();
-            if(next.type == Token.Type.EOF)
-                break;
-            System.out.println(next.type + "  ***  " + next.attribute);
-        }*/
     }
 }
